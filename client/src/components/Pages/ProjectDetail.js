99a82baaeb91e63ed1969032813d9c89a4491d03
Nicholas Green <nicholasgreen94@gmail.com>
2018-06-25 21:05:43 -0700

    Starting work on project page

--- client/src/components/Pages/ProjectDetail.js
+++ client/src/components/Pages/ProjectDetail.js
@@ -20,12 +20,26 @@ class ProjectDetail extends React.Component{
     }
   }
 
-
   render() {
     const state = this.state.data
     return (
         <div className="inner_page">
+        { state &&
           <h1>{state.name}</h1>
+        }
+        { state && state.tags &&
+          <div className="tag_wrapper">{
+            state.tags.map((tag, i) => {
+                if ( i === state.tags.length - 1 ) {
+                  return <span key={i} className="project_tags">{tag}</span>
+                } else {
+                  return <span key={i} className="project_tags">{tag + ','}</span>
+                }
+              })
+            }
+          </div>
+        }
+        <p className="project_description">{state.description}</p>
         </div>
       )
     }
